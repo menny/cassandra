c63096b18e066148d0039f813d62a2288b5e433f
Menny Even Danan <menny@evendanan.net>
2026-08-01 13:37:21 -0400

    test: use hermetic Go SDK toolchain in godoc test

--- tools/mcp_servers/godoc/server_test.go
+++ tools/mcp_servers/godoc/server_test.go
@@ -2,10 +2,39 @@ package godoc
 
 import (
 	"context"
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 
+	"github.com/bazelbuild/rules_go/go/runfiles"
 	"github.com/stretchr/testify/assert"
 )
+
+func init() {
+	if rpath := os.Getenv("GO_BIN_RLOCATION"); rpath != "" {
+		if absPath, err := runfiles.Rlocation(rpath); err == nil && absPath != "" {
+			idx := strings.Index(absPath, ".runfiles/")
+			if idx != -1 {
+				runfilesRoot := absPath[:idx+len(".runfiles")]
+				var foundGoBin string
+				_ = filepath.Walk(runfilesRoot, func(path string, info os.FileInfo, err error) error {
+					if err == nil && !info.IsDir() && info.Name() == "go" && strings.Contains(path, "go_sdk") {
+						foundGoBin = path
+						return filepath.SkipAll
+					}
+					return nil
+				})
+				if foundGoBin != "" {
+					absPath = foundGoBin
+				}
+			}
+			_ = os.Setenv("GO_BIN", absPath)
+			goroot := filepath.Dir(filepath.Dir(absPath))
+			_ = os.Setenv("GOROOT", goroot)
+		}
+	}
+}
 
 func TestRunGoDoc(t *testing.T) {
 	ctx := context.Background()
